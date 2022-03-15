d9a844565dc21221d38e81f2a377b41b09a6a4fd
Aman Mishra <aman777444@gmail.com>
2022-03-15 12:12:27 +0100

    Commit till Model Forms

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,50 @@
+from asyncio.windows_events import NULL
+from concurrent.futures.process import _python_exit
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    email=models.EmailField(max_length=200,null=True)
+    phone=models.IntegerField(null=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY=(
+        ('Indoor','Indoor'),
+        ('Outdoor','Outdoor')
+    )
+    name=models.CharField(max_length=200,null=True)
+    description=models.TextField(max_length=10000,null=True)
+    category=models.CharField(max_length=200,null=True,choices=CATEGORY)
+    price=models.FloatField(null=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    tags=models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending','Pending'),
+        ('Out for Delivery','Out for Delivery'),
+        ('Delivered','Delivered')
+    )
+    customer=models.ForeignKey(Customer, null = True , on_delete=models.SET_NULL)
+    product=models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    status=models.CharField(max_length=200,null=True,choices=STATUS)
+
